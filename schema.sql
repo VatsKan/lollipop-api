20669b60bdb34ce5a54f4219cbad110e5f2518be
Vatsal Kanoria <vatskan@gmail.com>
2020-12-27 16:40:50 +0800

    sql schema draft 2

--- schema.sql
+++ schema.sql
@@ -1,8 +1,10 @@
 BEGIN;
 
+DROP TABLE IF EXISTS teams, admin, members, scenarios, history CASCADE;
+
 CREATE TABLE admin (
   id SERIAL PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(225) NOT NULL
 );
 
@@ -16,33 +18,23 @@ CREATE TABLE teams (
 CREATE TABLE members (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(225) NOT NULL,
-  last_name VARCHAR(225) NOT NULL,
+  last_name VARCHAR(225),
   team_id INTEGER NOT NULL REFERENCES teams(id)
 );
 
 CREATE TABLE scenarios (
   id SERIAL PRIMARY KEY,
   name VARCHAR(225) NOT NULL,
-  team_id INTEGER NOT NULL REFERENCES teams(id)
-);
-
-CREATE TABLE team_member_scenario (
-  id SERIAL PRIMARY KEY,
   team_id INTEGER NOT NULL REFERENCES teams(id),
-  member_id INTEGER NOT NULL REFERENCES members(id),
-  scenario_id  INTEGER NOT NULL REFERENCES scenarios(id)
+  time_scenario_created_utc timestamp NOT NULL DEFAULT NOW()
 );
--- need to create a join table instead?
 
 CREATE TABLE history (
   id SERIAL PRIMARY KEY,
-  time_last_selected_utc VARCHAR(225) NOT NULL,
-  team_member_scenario_id INTEGER NOT NULL REFERENCES team_member_scenario(id)
-);
-
-CREATE TABLE lollipop_stack (
-  id SERIAL PRIMARY KEY,
-  team_member_scenario_id INTEGER NOT NULL REFERENCES team_member_scenario(id)
+  time_last_selected_utc timestamp NOT NULL,
+  team_id INTEGER NOT NULL REFERENCES teams(id),
+  member_id INTEGER NOT NULL REFERENCES members(id),
+  scenario_id INTEGER NOT NULL REFERENCES scenarios(id)
 );
 
 COMMIT;
